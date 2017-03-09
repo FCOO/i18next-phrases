b9e4aaf13f10782b20559d3a645b3a68a816ba11
Niels Holt <niels@steenbuchholt.dk>
2017-03-09 16:16:25 +0100

    First version

--- README.md
+++ README.md
@@ -1,2 +1,158 @@
 # i18next-phrases
+[i18next]:http://i18next.com
+
+
+## Description
 Extend i18next to load translations as { namespace: { key: { lang1: value1, lang2: value2 } } }
+
+## Installation
+### bower
+`bower install https://github.com/FCOO/i18next-phrases.git --save`
+
+## Demo
+http://FCOO.github.io/i18next-phrases/demo/ 
+
+## Usage
+Default 1-dim json-structure for [i18next] is `lang->namespace->key`:
+
+    { 
+        lang1: { 
+            namespace1: { 
+                key1: value1-1-1,
+                key2: value1-1-2
+             },
+            namespace2: { 
+                key1: value1-2-1,
+                key2: value1-2-2
+             }
+        },
+        lang2: { 
+            namespace1: { 
+                key1: value2-1-1,
+                key2: value2-1-2
+            },
+            namespace2: { 
+                key1: value2-2-1,
+                key2: value2-2-2
+             }
+        }
+    }
+
+To make adding translation easier two new formats are supported: 
+
+### phrase: `namespace->key->lang`
+    {
+        namespace1: {
+            key1: {
+                lang1: value1-1-1,
+                lang2: value2-1-1
+            },
+            key2: {
+                lang1: value1-1-2,
+                lang2: value2-1-2
+            }
+        },
+        namespace2: {
+            key1: {
+                lang1: value1-2-1,
+                lang2: value2-2-1
+            },
+            key2: {
+                lang1: value1-2-2,
+                lang2: value2-2-2
+            }
+        }
+    }
+    
+
+### key-phrase: `key->namespace->lang`
+    {
+        key1: {
+            namespace1: {
+                lang1: value1-1-1,
+                lang2: value2-1-1
+            },
+            namespace2: {
+                lang1: value1-2-1,
+                lang2: value2-2-1
+            }
+        },
+        key2: {
+            namespace1: {
+                lang1: value1-1-2,
+                lang2: value2-1-2
+            },
+            namespace2: {
+                lang1: value1-2-2,
+                lang2: value2-2-2
+            }
+        }
+    }
+
+
+
+## Methods
+The following methods are added to [i18next].
+
+### `i18next.addPhrase( key, [namespace,] langValues) `
+`key {string}` can be a combined namespace:key string. 
+`langValues = { [lang: value]xN }`
+
+### `i18next.addPhrases( [namespace,] keyLangValues )`
+`keyLangValues = { key: { [lang: value]xN }, key2: { [lang: value]xN } }`
+
+### `i18next.addBundlePhrases( namespaceKeyLangValues )`
+    namespaceKeyLangValues = {
+        namespace1: { 
+            key1: { [lang: value]xN }, 
+            key2: { [lang: value]xN }
+        }, 
+        namespace2:{
+            ...
+        }
+    }
+
+### `i18next.loadPhrases( jsonFileName, onFail )`
+`onFail ) function()`
+
+### `i18next.addKeyPhrase( key, namespace, langValues )`
+`langValues = { [lang: value]xN }`
+
+### `i18next.addKeyPhrases( key, namespaceLangValues ) `
+`namespaceLangValues = { namespace1: { [lang: value]xN }, namespace2: { [lang: value]xN } }`
+
+### `i18next.addBundleKeyPhrases( keyNamespaceLangValues ) `
+    keyNamespaceLangValues = { 
+        key1: { 
+            namespace1: { [lang: value]xN }, 
+            namespace2: { [lang: value]xN }
+        }, 
+        key2:{
+            ...
+        }
+    }`
+
+### `i18next.loadKeyPhrases( jsonFileName, onFail )`
+`onFail ) function()`
+
+### `i18next.sentence( langValues, options ) - return {string}`
+`langValues = { {lang: value}xN }`
+A single translation of a phrase. No key used or added. 
+E.g.
+
+    i18next.sentence({ da:'Dette er på dansk', en:'This is ín English'})
+
+### `i18next.s( langValues, options ) - return {string}`
+Same as `i18next.sentence`
+
+
+
+## Copyright and License
+This plugin is licensed under the [MIT license](https://github.com/FCOO/i18next-phrases/LICENSE).
+
+Copyright (c) 2017 [FCOO](https://github.com/FCOO)
+
+## Contact information
+
+Niels Holt nho@fcoo.dk
+
---
+++ src/i18next-phrases.js
@@ -0,0 +1,243 @@
+/****************************************************************************
+    i18next-phrases.js, 
+
+    (c) 2017, FCOO
+
+    https://github.com/FCOO/i18next-phrases
+    https://github.com/FCOO
+
+****************************************************************************/
+
+/****************************************************************************
+    Default 1-dim json-structure for i18next is 
+        { lang1: { 
+            namespace: { key: value1 }
+          },
+          lang2: { 
+            namespace: { key: value2 }
+          }
+        }
+
+    To make adding translation easier two new formats are supported: 
+
+    1: phrase: namespace->key->lang
+        {
+            namespace1: {
+                key1: {
+                    lang1: value1-1-1,
+                    lang2: value2-1-1
+                },
+                key2: {
+                    lang1: value1-1-2,
+                    lang2: value2-1-2
+                }
+            },
+            namespace2: {
+                key1: {
+                    lang1: value1-2-1,
+                    lang2: value2-2-1
+                },
+                key2: {
+                    lang1: value1-2-2,
+                    lang2: value2-2-2
+                }
+            }
+        }
+    
+    2: key-phrase: key->namespace->lang
+        {
+            key1: {
+                namespace1: {
+                    lang1: value1-1-1,
+                    lang2: value2-1-1
+                },
+                namespace2: {
+                    lang1: value1-2-1,
+                    lang2: value2-2-1
+                }
+            },
+            key2: {
+                namespace1: {
+                    lang1: value1-1-2,
+                    lang2: value2-1-2
+                },
+                namespace2: {
+                    lang1: value1-2-2,
+                    lang2: value2-2-2
+                }
+            }
+        }
+****************************************************************************/
+
+(function ($, i18next/*, window, document, undefined*/) {
+    "use strict";
+
+    /***********************************************************************
+    addPhrase( key, [namespace,] langValues) 
+    - key {string} can be a combined namespace:key string. 
+    - langValues = { [lang: value]xN }
+    ***********************************************************************/
+    i18next.addPhrase = function( namespace, key, langValues ){
+        var nsSeparator = this.options.nsSeparator,
+            _this = this;
+
+        if (arguments.length == 2){
+            langValues = arguments[1];
+            if (arguments[0].indexOf(nsSeparator) > -1){
+              key = arguments[0].split(nsSeparator)[1];
+              namespace = arguments[0].split(nsSeparator)[0];
+            }
+            else {
+                key = arguments[0];
+                namespace = this.options.defaultNS[0];
+            }
+        }
+
+        $.each( langValues, function( lang, value ){
+            _this.addResource(lang, namespace, key, value);
+        });
+        return this;
+    };
+
+    /***********************************************************************
+    addPhrases( [namespace,] keyLangValues )
+    - keyLangValues = { key: [lang: value]xN }, key2: [lang: value]xN } }
+    ***********************************************************************/
+    i18next.addPhrases = function( namespace, keyLangValues ){
+        var _this = this;
+        $.each( keyLangValues, function( key, langValues ){
+            _this.addPhrase( namespace, key, langValues );
+        });
+        return this;
+    };
+
+    /***********************************************************************
+    addBundlePhrases( namespaceKeyLangValues  )
+    - namespaceKeyLangValues = {
+          namespace1: { 
+              key1: { [lang: value]xN }, 
+              key2: { [lang: value]xN }
+          }, 
+          namespace2:{
+              ...
+          }
+      }
+    ***********************************************************************/
+    i18next.addBundlePhrases = function( namespaceKeyLangValues ){
+        var _this = this;
+        $.each( namespaceKeyLangValues, function( namespace, keyLangValues ){
+            _this.addPhrases( namespace, keyLangValues );
+        });
+        return this;
+    };
+
+    /***********************************************************************
+    i18next.loadPhrases( jsonFileName );
+    ***********************************************************************/
+    i18next.loadPhrases = function( jsonFileName, onFail ){
+        var jqxhr = $.getJSON( jsonFileName ),
+            _this = this;
+        if (onFail)
+            jqxhr.fail( onFail );
+
+        jqxhr.done( function( data ) {
+            _this.addBundlePhrases( data );
+        });
+    };
+    
+    /***********************************************************************
+    i18next.addKeyPhrase = function( key, namespace, langValues )
+    - langValues = { [lang: value]xN }
+    ***********************************************************************/
+    i18next.addKeyPhrase = function( key, namespace, langValues ){
+        return this.addPhrase( namespace, key, langValues );
+    };
+
+    /***********************************************************************
+    i18next.addKeyPhrases = function( key, namespaceLangValues )
+    - namespaceLangValues = { 
+          namespace1: { [lang: value]xN }, 
+          namespace2: { [lang: value]xN } }, 
+      }
+    ***********************************************************************/
+    i18next.addKeyPhrases = function( key, namespaceLangValues ){
+        var _this = this;
+        $.each( namespaceLangValues, function( namespace, langValues ){
+            _this.addKeyPhrase( key, namespace, langValues );
+        });
+        return this;
+    };
+
+    /***********************************************************************
+    addBundleKeyPhrases( keyNamespaceLangValues  )
+    keyNamespaceLangValues = { 
+        key1: { 
+            namespace1: { [lang: value]xN }, 
+            namespace2: { [lang: value]xN }
+        }, 
+        key2:{
+            ...
+        }
+    }`
+    ***********************************************************************/
+    i18next.addBundleKeyPhrases = function( keyNamespaceLangValues ){
+        var _this = this;
+        $.each( keyNamespaceLangValues, function( key, namespaceLangValues ){
+            _this.addKeyPhrases( key, namespaceLangValues );
+        });
+        return this;
+    };
+
+    /***********************************************************************
+    i18next.loadKeyPhrases = function( jsonFileName, onFail )
+    ***********************************************************************/
+    i18next.loadKeyPhrases = function( jsonFileName, onFail ){
+        var jqxhr = $.getJSON( jsonFileName );
+        if (onFail)
+            jqxhr.fail( onFail );
+
+        jqxhr.done( function( data ) {
+            $.each( data, function( namespace, keyLangValues ) {
+                i18next.addKeyPhrases( namespace, keyLangValues );
+            });
+        });
+    
+    };
+    
+    
+    /***********************************************************************
+    sentence ( langValues, options )
+    - langValues = { [lang: value]xN }
+    A single translation of a sentence. No key used or added
+    ***********************************************************************/
+    i18next.sentence = function( langValues, options ){ 
+        var nsTemp = '__TEMP__',
+            keyTemp = '__KEY__',
+            _this = this,
+            nsSeparator = this.options.nsSeparator;
+        
+        //Remove any data from nsTemp
+//        $.each( languages, function( index, lang ){
+//            _this.removeResourceBundle(lang, nsTemp);
+//        });
+        this.addPhrase( nsTemp, keyTemp, langValues );
+        var result = this.t(nsTemp + nsSeparator + keyTemp, options );
+
+        //Remove the key again
+        $.each( langValues, function( lang ){
+            _this.removeResourceBundle(lang, nsTemp);
+        });
+
+        return result;
+    };
+
+    /***********************************************************************
+    s ( langValues, options )
+    - langValues = { [lang: value]xN }
+    ***********************************************************************/
+    i18next.s = function( langValues, options ){
+        return this.sentence( langValues, options );
+    };
+
+
+}(jQuery, this.i18next, this, document));
